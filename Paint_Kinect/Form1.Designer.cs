ec7b1fba28605545a3aeefdb03d6d33037fb71f7
Víctor Castillo Rodríguez <victorcas14@hotmail.com>
2017-05-18 13:18:39 +0200

    Kinect Paint. Detects 2 or more Skeletons. Its posible to draw over the image when the right hand is 40cm nearer than the shoulders center

---
+++ Paint_Kinect/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Paint_Kinect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PB_Lienzo = new System.Windows.Forms.PictureBox();
+            this.B_conect = new System.Windows.Forms.Button();
+            this.Status_kinect = new System.Windows.Forms.Label();
+            this.PB_Lienzo2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Lienzo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Lienzo2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PB_Lienzo
+            // 
+            this.PB_Lienzo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PB_Lienzo.BackColor = System.Drawing.Color.White;
+            this.PB_Lienzo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PB_Lienzo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.PB_Lienzo.Location = new System.Drawing.Point(12, 56);
+            this.PB_Lienzo.Name = "PB_Lienzo";
+            this.PB_Lienzo.Size = new System.Drawing.Size(782, 415);
+            this.PB_Lienzo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB_Lienzo.TabIndex = 0;
+            this.PB_Lienzo.TabStop = false;
+            this.PB_Lienzo.SizeChanged += new System.EventHandler(this.PB_Lienzo_SizeChanged);
+            // 
+            // B_conect
+            // 
+            this.B_conect.Location = new System.Drawing.Point(13, 13);
+            this.B_conect.Name = "B_conect";
+            this.B_conect.Size = new System.Drawing.Size(75, 23);
+            this.B_conect.TabIndex = 1;
+            this.B_conect.Text = "Conectar";
+            this.B_conect.UseVisualStyleBackColor = true;
+            this.B_conect.Click += new System.EventHandler(this.B_conect_Click);
+            // 
+            // Status_kinect
+            // 
+            this.Status_kinect.AutoSize = true;
+            this.Status_kinect.Location = new System.Drawing.Point(13, 478);
+            this.Status_kinect.Name = "Status_kinect";
+            this.Status_kinect.Size = new System.Drawing.Size(0, 13);
+            this.Status_kinect.TabIndex = 2;
+            // 
+            // PB_Lienzo2
+            // 
+            this.PB_Lienzo2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PB_Lienzo2.BackColor = System.Drawing.Color.Transparent;
+            this.PB_Lienzo2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PB_Lienzo2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PB_Lienzo2.ErrorImage = null;
+            this.PB_Lienzo2.InitialImage = null;
+            this.PB_Lienzo2.Location = new System.Drawing.Point(12, 56);
+            this.PB_Lienzo2.Name = "PB_Lienzo2";
+            this.PB_Lienzo2.Size = new System.Drawing.Size(782, 415);
+            this.PB_Lienzo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB_Lienzo2.TabIndex = 3;
+            this.PB_Lienzo2.TabStop = false;
+            this.PB_Lienzo2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PB_Lienzo2_MouseDown);
+            this.PB_Lienzo2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PB_Lienzo2_MouseMove);
+            this.PB_Lienzo2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PB_Lienzo2_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 497);
+            this.Controls.Add(this.PB_Lienzo2);
+            this.Controls.Add(this.Status_kinect);
+            this.Controls.Add(this.B_conect);
+            this.Controls.Add(this.PB_Lienzo);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kinect Paint";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Lienzo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Lienzo2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PB_Lienzo;
+        private System.Windows.Forms.Button B_conect;
+        private System.Windows.Forms.Label Status_kinect;
+        private System.Windows.Forms.PictureBox PB_Lienzo2;
+    }
+}
+
---
+++ Paint_Kinect/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+
+namespace Paint_Kinect
+{
+    public partial class Form1 : Form
+    {
+        private KinectSensor sensor;
+        //private Skeleton[] esqueletos;
+        private float escalay;
+        private float escalax;
+        private Color color_elejido = Color.Red;
+        private int grosor = 25;
+        private Boolean click = false;
+        private double tolerancia = 0.40;
+        Graphics g;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PB_Lienzo2.Parent = PB_Lienzo;
+            g = PB_Lienzo2.CreateGraphics();
+            PB_Lienzo2.Location = new Point(0, 0);
+            PB_Lienzo2.Image = new Bitmap(PB_Lienzo.Width, PB_Lienzo.Height);
+            this.escalax = (float)PB_Lienzo.Width / 640;
+            this.escalay = (float)PB_Lienzo.Height / 480;
+            reconect();
+        }
+
+        private void B_conect_Click(object sender, EventArgs e)
+        {
+            reconect();
+        }
+
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] esqueletos = new Skeleton[0];
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    esqueletos = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(esqueletos);
+                }
+            }
+            if (esqueletos.Length != 0)
+            {
+                foreach (Skeleton skel in esqueletos)
+                {
+                    float x = sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skel.Joints[JointType.HandRight].Position, DepthImageFormat.Resolution640x480Fps30).X;
+                    float y = sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skel.Joints[JointType.HandRight].Position, DepthImageFormat.Resolution640x480Fps30).Y;
+                    x = (int)(Math.Abs(x * escalax) - PB_Lienzo2.Location.X);
+                    y = (int)(Math.Abs(y * escalay) - PB_Lienzo2.Location.Y);
+                    Status_kinect.Text = "X: " + x + "   Y: " + y;
+                    if (x != 0 && y != 0)
+                    {
+                        if (skel.Joints[JointType.ShoulderCenter].Position.Z - skel.Joints[JointType.HandRight].Position.Z > tolerancia)
+                        {
+                            Graphics.FromImage(PB_Lienzo2.Image).FillEllipse(new SolidBrush(color_elejido), x, y, grosor, grosor);
+                            PB_Lienzo2.Invalidate();
+                        }
+                        else
+                        {
+                            g.DrawEllipse(new Pen(color_elejido, 12), new Rectangle((int)(x - (25)), (int)(y - (25)), 50, 50));
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private void Sensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (var frame = e.OpenColorImageFrame())
+            {
+                if (frame != null)
+                {
+                    PB_Lienzo.Image = crearbitmap(frame);
+                    //PB_Lienzo2.Refresh();
+                }
+            }
+        }
+
+        private Bitmap crearbitmap(ColorImageFrame fotograma)
+        {
+            byte[] pixeldata = new byte[fotograma.PixelDataLength];
+            fotograma.CopyPixelDataTo(pixeldata);
+            Bitmap bmap = new Bitmap(fotograma.Width, fotograma.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            System.Drawing.Imaging.BitmapData bmapdata = bmap.LockBits(
+                new Rectangle(0, 0, fotograma.Width, fotograma.Height),
+                System.Drawing.Imaging.ImageLockMode.WriteOnly,
+                bmap.PixelFormat);
+            IntPtr ptr = bmapdata.Scan0;
+            System.Runtime.InteropServices.Marshal.Copy(pixeldata, 0, ptr, fotograma.PixelDataLength);
+            bmap.UnlockBits(bmapdata);
+            return bmap;
+        }
+
+        private void PB_Lienzo_SizeChanged(object sender, EventArgs e)
+        {
+            this.escalax = (float)PB_Lienzo.Width / 640;
+            this.escalay = (float)PB_Lienzo.Height / 480;
+            PB_Lienzo2.Refresh();
+        }
+        private void PB_Lienzo2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (click)
+            {
+                Graphics.FromImage(PB_Lienzo2.Image).FillEllipse(new SolidBrush(color_elejido), Cursor.Position.X - PB_Lienzo.Location.X - this.Location.X-17, Cursor.Position.Y - PB_Lienzo.Location.Y - this.Location.Y-40, grosor, grosor);
+                PB_Lienzo2.Invalidate();
+                Status_kinect.Text = "X: " + Cursor.Position.X + "   Y: " + PB_Lienzo.Location.X + "        " + this.Location.X;
+            }
+        }
+        private void reconect()
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                sensor = KinectSensor.KinectSensors[0];
+            }
+            else
+            {
+                return;
+            }
+            sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+            sensor.ColorFrameReady += Sensor_ColorFrameReady;
+            sensor.SkeletonStream.Enable();
+            this.sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+            try
+            {
+                this.sensor.Start();
+            }
+            catch (System.IO.IOException)
+            {
+                this.sensor = null;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        private void PB_Lienzo2_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void PB_Lienzo2_MouseDown(object sender, MouseEventArgs e)
+        {
+            click = true;
+        }
+
+        private void PB_Lienzo2_MouseUp(object sender, MouseEventArgs e)
+        {
+            click = false;
+        }
+    }
+}
